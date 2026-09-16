92e775d7276fd9ae8480962a1fc4b66c5eb4b616
arielmorel <arielmoreld@gmail.com>
2026-09-16 08:59:47 -0400

    feat: add explicit OAI-SearchBot allow rule to robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/admin", "/private"],
-    },
+    rules: [
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/private"],
+      },
+    ],
     sitemap: "https://moreltechnologyrd.com/sitemap.xml",
   };
 }
